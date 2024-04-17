2de6abdff1c1deb96d70edefdd0c73d0f6393908
256995 <256995@vutbr.cz>
2024-04-17 09:18:17 +0200

    add selection sorting

--- sorting.py
+++ sorting.py
@@ -15,16 +15,37 @@ def read_data(file_name):
     with open(file_path, mode="r") as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
-            for key in row.keys():
+            for key in row:
                 if key not in data:
                     data[key] = []
                 data[key].append(int(row[key]))
     return data
 
 
+def selection_sort(my_list):
+    """
+    selection sorting
+    :param my_list: unordered list of int
+    :return: my_list (ordered list of int)
+    """
+    right_border = 0
+    while right_border < len(my_list) - 1:
+        min_no = my_list[right_border]
+        min_idx = 0
+        for idx, no in enumerate(my_list[right_border:]):
+            if no < min_no:
+                min_no = no
+                min_idx = idx
+        my_list[right_border], my_list[min_idx + right_border] = my_list[min_idx + right_border], my_list[right_border]
+        right_border += 1
+    return my_list
+
+
 def main():
     numbers = read_data("numbers.csv")
     print(numbers)
+    sorted_list = selection_sort(numbers['series_1'])
+    print(sorted_list)
 
 
 if __name__ == '__main__':
